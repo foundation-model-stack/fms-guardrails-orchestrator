2315ec871fd177d346da9a758a7798183d234d49
gkumbhat <kumbhat.gaurav@gmail.com>
2024-04-17 15:29:57 -0500

    :sparkles: Add streaming server endpoint with dummy response
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,4 @@
 use std::{net::SocketAddr, time::Duration};
-
 use axum::{
     extract::Extension,
     http::{HeaderMap, Method, StatusCode},
@@ -7,10 +6,14 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use futures::stream::Any;
+// sse -> server side events
+use axum::response::sse::{Event, KeepAlive, Sse};
+use futures::stream::Stream;
+use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use tokio::{fs::read, signal, time::sleep};
 use tracing::info;
+use std::convert::Infallible;
 
 
 const API_PREFIX: &'static str = r#"/api/v1/task"#;
@@ -33,7 +36,8 @@ pub async fn run(
     // Build and await on the HTTP server
     let app = Router::new()
         .route("/health", get(health))
-        .route(&format!("{}/classification-with-text-generation", API_PREFIX), post(classification_with_generation));
+        .route(&format!("{}/classification-with-text-generation", API_PREFIX), post(classification_with_generation))
+        .route(&format!("{}/server-streaming-classification-with-text-generation", API_PREFIX), post(stream_classification_with_gen));
 
     let server = axum::Server::bind(&rest_addr)
         .serve(app.into_make_service())
@@ -58,12 +62,58 @@ struct sample_response {
     response: inner_response
 }
 
+// TODO: Dummy streaming response object
+#[derive(Serialize)]
+pub(crate) struct StreamResponse {
+    pub generated_text: String,
+    pub processed_index: i32,
+}
+
+const DUMMY_RESPONSE: [&'static str; 9] = ["This", "is", "very", "good", "news,", "streaming", "is", "working", "!"];
+
 async fn classification_with_generation(Json(payload): Json<Value>) ->  (StatusCode, Json<Value>) {
     // TODO: determine how to detect if orchestrator is healthy or not
     let response = json!({"response": {"sample": true}});
     (StatusCode::OK, Json(response))
 }
 
+
+async fn stream_classification_with_gen(Json(payload): Json<Value>) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+
+    let on_message_callback = |stream_token: StreamResponse| {
+        let event = Event::default();
+        event.json_data(stream_token).unwrap()
+    };
+
+    let (response_stream) =
+        generate_stream_response(Json(payload), on_message_callback).await;
+    let sse = Sse::new(response_stream).keep_alive(KeepAlive::default());
+    sse
+}
+
+async fn generate_stream_response(
+    Json(payload): Json<Value>,
+    on_message_callback: impl Fn(StreamResponse) -> Event,
+) -> impl Stream<Item = Result<Event, Infallible>> {
+
+
+    let mut dummy_response_iterator = DUMMY_RESPONSE.iter();
+
+    let mut index: i32 = 0;
+    let stream = async_stream::stream! {
+        // Server sending event stream
+        while let Some(&token) = dummy_response_iterator.next() {
+            let stream_token = StreamResponse {
+                generated_text: token.to_string(),
+                processed_index: index
+            };
+            index += 1;
+            let event = on_message_callback(stream_token);
+            yield Ok(event);
+        }
+    };
+    stream
+}
 
 
 /// Shutdown signal handler
