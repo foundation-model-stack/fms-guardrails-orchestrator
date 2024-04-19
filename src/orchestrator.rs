44561e8ee72a5c2d1b90817ddcbbc2e8d06bccf3
Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
2024-04-19 14:31:34 -0600

    :construction: Begin orchestration input detection
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>

--- src/orchestrator.rs
+++ src/orchestrator.rs
@@ -1,6 +1,6 @@
-use std::{collections::{hash_map::Entry, HashMap}, usize};
+use std::{collections::HashMap, usize};
 
-use crate::{config::{ChunkerConfig, ChunkerType, DetectorConfig, DetectorMap}, models::{GuardrailsConfig, GuardrailsHttpRequest}, pb::fmaas::{generation_service_server::GenerationService, BatchedTokenizeRequest, TokenizeRequest}, ErrorResponse};
+use crate::{config::{ChunkerConfig, ChunkerType, DetectorConfig, DetectorMap}, models::{GuardrailsConfig, GuardrailsHttpRequest, GuardrailsTextGenerationParameters}, pb::fmaas::{generation_service_server::GenerationService, BatchedTokenizeRequest, TokenizeRequest}, ErrorResponse};
 use axum::{
     response::IntoResponse,
     Json,
@@ -32,6 +32,25 @@ pub(crate) struct TokenizeResponse {
     // ...
 }
 
+// TODO: Dummy chunker response objects - replace later
+#[derive(Clone, Serialize)]
+pub(crate) struct Token {
+    pub start: u32,
+    pub end: u32,
+    pub text: String
+}
+#[derive(Clone, Serialize)]
+pub(crate) struct TokenizationResults {
+    pub results: Vec<Token>,
+}
+
+#[derive(Clone, Serialize)]
+pub(crate) struct TokenizationStreamResult {
+    pub results: TokenizationResults,
+    pub processed_index: u32,
+    pub start_index: u32,
+}
+
 // TODO: Dummy detector response objects - replace later
 #[derive(Serialize)]
 pub(crate) struct DetectorResult {
@@ -52,7 +71,8 @@ const DUMMY_RESPONSE: [&'static str; 9] = ["This", "is", "very", "good", "news,"
 
 // ========================================== Handler functions ==========================================
 
-
+// This is designed to be boot-time validation and does not have to persist here
+// The results should get processed before tasks are called to raise an error for config
 pub fn preprocess_detector_map(detector_map: DetectorMap) -> Result<(HashMap<String, String>, HashMap<String, Result<ChunkerConfig, ErrorResponse>>), ErrorResponse> {
     // Map detectors to respective chunkers
     let chunkers: HashMap<String, ChunkerConfig> = detector_map.chunkers;
@@ -75,16 +95,24 @@ pub fn preprocess_detector_map(detector_map: DetectorMap) -> Result<(HashMap<Str
 
 // ========================================== Dummy Tasks ==========================================
 
-// API calls
+// API calls - do not have to actually live here
+
+// Unary TGIS call
+async fn tgis_unary_call(model_id: String, texts: Vec<String>, text_gen_params: Option<GuardrailsTextGenerationParameters>) -> GenerationResponse {
+    GenerationResponse {
+        input_token_count: 1,
+        generated_token_count: 1, 
+        text: "hi".to_string(),
+    }
+}
 
 // Server streaming TGIS call
+// TODO: update payload here
 async fn tgis_stream_call(
     Json(tgis_payload): Json<GuardrailsHttpRequest>,
+    text_gen_params: Option<GuardrailsTextGenerationParameters>,
     on_message_callback: impl Fn(GenerationResponse) -> Event,
 ) -> impl Stream<Item = Result<Event, Infallible>> {
-
-    const DUMMY_RESPONSE: [&'static str; 9] = ["This", "is", "very", "good", "news,", "streaming", "is", "working", "!"];
-
     let mut dummy_response_iterator = DUMMY_RESPONSE.iter();
 
     let mut input_token_count: u32 = 0;
@@ -106,7 +134,7 @@ async fn tgis_stream_call(
 }
 
 // Unary TGIS tokenize call
-async fn tokenize_call(model_id: String, texts: Vec<String>) -> TokenizeResponse {
+async fn tokenize_unary_call(model_id: String, texts: Vec<String>) -> TokenizeResponse {
     let mut tokenize_requests: Vec<TokenizeRequest> = vec![];
     for text in texts.iter() {
         let tokenize_request: TokenizeRequest = TokenizeRequest { text: text.to_string() };
@@ -126,9 +154,59 @@ async fn tokenize_call(model_id: String, texts: Vec<String>) -> TokenizeResponse
     }
 }
 
+// Unary chunker call
+async fn chunker_unary_call(model_id: String, text: String) -> TokenizationResults {
+    // unary under the hood
+    let token_0 = Token {
+        start: 0,
+        end: 4,
+        text: "This".to_string(),
+    };
+    let token_1 = Token {
+        start: 5,
+        end: 7,
+        text: "is".to_string(),
+    };
+    TokenizationResults {
+        results: vec![token_0, token_1]
+    }
+}
+
+// Bidirectional streaming chunker call
+async fn chunker_stream_call(model_id: String, texts: Vec<String>, on_message_callback: impl Fn(TokenizationStreamResult) -> Event) -> impl Stream<Item = Result<Event, Infallible>> {
+    let token_0 = Token {
+        start: 0,
+        end: 4,
+        text: "This".to_string(),
+    };
+    let token_1 = Token {
+        start: 5,
+        end: 7,
+        text: "is".to_string(),
+    };
+    let tok_results = TokenizationResults {
+        results: vec![token_0, token_1]
+    };
+    let mut dummy_response_iterator = DUMMY_RESPONSE.iter();
+
+    let stream = async_stream::stream! {
+        // Server sending event stream
+        while let Some(&token) = dummy_response_iterator.next() {
+            let stream_token = TokenizationStreamResult {
+                results: tok_results.clone(),
+                processed_index: 1, 
+                start_index: 0 
+            };
+            let event = on_message_callback(stream_token);
+            yield Ok(event);
+        }
+    };
+    stream
+}
+
 // Unary detector call
 // Assume processing on batch (multiple strings) can at least happen
-async fn detector_call(model_id: String, inputs: Vec<String>) -> DetectorResponse {
+async fn detector_call(detector_id: String, inputs: Vec<String>) -> DetectorResponse {
     // Might need some routing/extra endpoint info to begin with
     let result: DetectorResult = DetectorResult {
         start: 0,
@@ -160,13 +238,38 @@ fn slice_input(mut user_input: Vec<String>, payload: GuardrailsHttpRequest) -> V
     user_input
 }
 
-async fn input_detection(input_detectors_models: HashMap<String, HashMap<String, String>>) {
-    // TODO
+async fn input_detection(
+    input_detectors_models: HashMap<String, HashMap<String, String>>,
+    chunker_map: HashMap<String, String>,
+    chunker_config_map: HashMap<String, Result<ChunkerConfig, ErrorResponse>>,
+    texts: Vec<String>,
+) -> Vec<DetectorResponse> {
+    // input_detectors_models: model_name: {param: value}
+    // Future - parallelize calls
+    let mut detector_responses = Vec::new();
+    for detector_id in input_detectors_models.keys() {
+        if let Some(chunker_id) = chunker_map.get(detector_id){
+            for text in texts.iter() {
+                // TODO: Get config/type for chunker call
+                let tokenization_results = chunker_unary_call(chunker_id.to_string(), text.to_string()).await;
+                // Optimize later - chunkers would always be called even if multiple detectors had same chunker
+                let mut texts: Vec<String> = Vec::new();
+                for token in tokenization_results.results.iter() {
+                    texts.push(token.text.to_string())
+                }
+                detector_responses.push(detector_call(detector_id.to_string(), texts).await)
+            }
+        } else {
+            continue;
+        }
+    }
+    detector_responses
 }
 
 // ========================================== Main ==========================================
 
-pub async fn create_tasks(payload: GuardrailsHttpRequest) {
+// In the future probably create DAG/list of tasks to be invoked
+pub async fn do_tasks(payload: GuardrailsHttpRequest, detector_hashmaps: (HashMap<String, String>, HashMap<String, Result<ChunkerConfig, ErrorResponse>>),streaming: bool) {
     // TODO: is clone() needed for every payload use? Otherwise move errors since payload has String
 
     // LLM / text generation model
@@ -187,6 +290,11 @@ pub async fn create_tasks(payload: GuardrailsHttpRequest) {
     // tracked as part of DAG/list instead of function in the future
     user_input = slice_input(user_input, payload.clone());
 
+    // Process detector hashmaps
+    let chunker_map: HashMap<String, String> = detector_hashmaps.0;
+    // Should ideally just be HashMap<String, ChunkerConfig> after processing
+    let chunker_config_map: HashMap<String, Result<ChunkerConfig, ErrorResponse>> = detector_hashmaps.1;
+
     // Check for input detection
     let input_detectors: Option<HashMap<String, HashMap<String, String>>> = payload.clone().guardrail_config.unwrap().input.unwrap().models;
     let do_input_detection: bool = input_detectors.is_some();
@@ -196,49 +304,50 @@ pub async fn create_tasks(payload: GuardrailsHttpRequest) {
         // or spliced user input (for masks) - latter today
         // This separate call would not be necessary if generation is called, since it
         // provides input_token_count
-        let input_token_count = tokenize_call(model_id, user_input);
+        // Add tokenization task to count input tokens - grpc [unary] call
+        let input_token_count = tokenize_unary_call(model_id.clone(), user_input.clone()).await;
         
         let input_detector_models: HashMap<String, HashMap<String, String>> = input_detectors.unwrap();
-        //let input_response = input_detection(input_detectors_models);
+        // Add detection task for each detector - rest [unary] call
+        // For each detector, add chunker task as precursor - grpc [unary] call
+        let input_response = input_detection(input_detector_models, chunker_map, chunker_config_map, user_input.clone()).await;
     }
-    // Add tokenization task to count input tokens - grpc [unary] call
-    // Get any detectors from payload.guardrail_config.input.models
-    // Add detection task for each detector - rest [unary] call
-    // For each detector, add chunker task as precursor - grpc [unary] call
 
     // Response aggregation task
     // "break" if input detection - but this fn not responsible for short-circuit
 
-    // payload.text_gen_parameters - extra TGIS generation parameters
+
+    // Check for output detection
+    let output_detectors: Option<HashMap<String, HashMap<String, String>>> = payload.clone().guardrail_config.unwrap().output.unwrap().models;
+    let do_output_detection: bool = output_detectors.is_some();
 
     // ============= Unary endpoint =============
-    // Add TGIS generation task - grpc [unary] call
-    // If output detection
-    // Get any detectors from payload.guardrail_config.output.models
-    // Add detection task for each detector - rest [unary] call
-    // For each detector, add chunker task as precursor - grpc [unary] call
-    // Response aggregation task
+    if !streaming {
+        // Add TGIS generation - grpc [unary] call
+        let tgis_response = tgis_unary_call(model_id.clone(), user_input.clone(), payload.text_gen_parameters).await;
+        if do_output_detection {
+            let output_detector_models: HashMap<String, HashMap<String, String>> = output_detectors.unwrap();
+            // Add detection task for each detector - rest [unary] call
+            // For each detector, add chunker task as precursor - grpc [unary] call
+        }
+        // Response aggregation task
 
+    } else { // ============= Streaming endpoint =============
+        // Add TGIS generation task - grpc [server streaming] call
+        let on_message_callback = |stream_token: GenerationResponse| {
+            let event = Event::default();
+            event.json_data(stream_token).unwrap()
+        };
 
+        // Fix payload here
+        let tgis_response_stream =
+            tgis_stream_call(Json(payload.clone()), payload.text_gen_parameters, on_message_callback, ).await;
 
-    // ============= Streaming endpoint =============
-    // Add TGIS generation task - grpc [server streaming] call
-    let on_message_callback = |stream_token: GenerationResponse| {
-        let event = Event::default();
-        event.json_data(stream_token).unwrap()
-    };
-
-    // Fix payload here
-    let tgis_response_stream =
-        tgis_stream_call(Json(payload.clone()), on_message_callback).await;
-
-
-    // If output detection
-    // Get any detectors from payload.guardrail_config.output.models
-    // Add detection task for each detector - rest [unary] call
-    // For each detector, add chunker task as precursor - grpc [bidi stream] call
-    // Response aggregation task
-
-    // Eventually make this into a DAG/list of tasks that will be invoked for
-    // abstraction purposes instead of calling functions directly here
+        if do_output_detection {
+            let output_detector_models: HashMap<String, HashMap<String, String>> = output_detectors.unwrap();
+            // Add detection task for each detector - rest [unary] call
+            // For each detector, add chunker task as precursor - grpc [bidi stream] call
+        }
+        // Response aggregation task
+    }
 }
