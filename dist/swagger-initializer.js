8f31230a52cc2f2e92645945a6c1b8e512872c73
gkumbhat <kumbhat.gaurav@gmail.com>
2024-05-20 14:56:42 -0500

    :construction: Add detector API support
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -2,9 +2,24 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
+  window.ui_orchest8 = SwaggerUIBundle({
     url: "docs/api/orchestrator_openapi_0_1_0.yaml",
-    dom_id: '#swagger-ui',
+    dom_id: '#orchestrator-api',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui_detectors = SwaggerUIBundle({
+    url: "docs/api/openapi_detector_api.yaml",
+    dom_id: '#detector-api',
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
