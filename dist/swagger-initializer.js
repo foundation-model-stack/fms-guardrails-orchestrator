747acf818232d154a26453a3623a5ad80ed119e8
gkumbhat <kumbhat.gaurav@gmail.com>
2024-05-20 15:09:25 -0500

    :construction: Fix 2 div mixup issue
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,10 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui_orchest8 = SwaggerUIBundle({
-    url: "docs/api/orchestrator_openapi_0_1_0.yaml",
+    urls: [
+      {url: "docs/api/orchestrator_openapi_0_1_0.yaml", name: "Orchestrator API"},
+      {url: "docs/api/openapi_detector_api.yaml", name: "Detector API"},
+    ],
     dom_id: '#orchestrator-api',
     deepLinking: true,
     presets: [
@@ -17,19 +20,19 @@ window.onload = function() {
   });
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui_detectors = SwaggerUIBundle({
-    url: "docs/api/openapi_detector_api.yaml",
-    dom_id: '#detector-api',
-    deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
+  // window.ui_detectors = SwaggerUIBundle({
+  //   url: "docs/api/openapi_detector_api.yaml",
+  //   dom_id: '#detector-api',
+  //   deepLinking: true,
+  //   presets: [
+  //     SwaggerUIBundle.presets.apis,
+  //     SwaggerUIStandalonePreset
+  //   ],
+  //   plugins: [
+  //     SwaggerUIBundle.plugins.DownloadUrl
+  //   ],
+  //   layout: "StandaloneLayout"
+  // });
 
   //</editor-fold>
 };
