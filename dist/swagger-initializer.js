3f1fef256e5d29eb8558a77215d681b466ead37e
gkumbhat <kumbhat.gaurav@gmail.com>
2024-05-20 15:43:56 -0500

    :technologist::sparkles: Add swagger ui for orchestrator and detector APIs
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,7 +1,5 @@
 window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui_orchest8 = SwaggerUIBundle({
     urls: [
       {url: "docs/api/orchestrator_openapi_0_1_0.yaml", name: "Orchestrator API"},
@@ -18,21 +16,4 @@ window.onload = function() {
     ],
     layout: "StandaloneLayout"
   });
-
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  // window.ui_detectors = SwaggerUIBundle({
-  //   url: "docs/api/openapi_detector_api.yaml",
-  //   dom_id: '#detector-api',
-  //   deepLinking: true,
-  //   presets: [
-  //     SwaggerUIBundle.presets.apis,
-  //     SwaggerUIStandalonePreset
-  //   ],
-  //   plugins: [
-  //     SwaggerUIBundle.plugins.DownloadUrl
-  //   ],
-  //   layout: "StandaloneLayout"
-  // });
-
-  //</editor-fold>
 };
