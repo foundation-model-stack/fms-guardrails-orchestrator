e9b350689d6773f24dd1e6612eddc62d3334869a
gkumbhat <kumbhat.gaurav@gmail.com>
2024-07-03 17:04:33 -0500

    :art: Fix formatting
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- src/clients.rs
+++ src/clients.rs
@@ -1,4 +1,4 @@
-/* 
+/*
  Copyright FMS Guardrails Orchestrator Authors
 
  Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 
-*/ 
+*/
 
 #![allow(dead_code)]
 use std::{collections::HashMap, time::Duration};
