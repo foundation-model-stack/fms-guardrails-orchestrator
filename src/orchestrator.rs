b9857e99e0d8ec154880c39e0f92f5f95f5407eb
gkumbhat <kumbhat.gaurav@gmail.com>
2024-07-31 10:48:20 -0500

    Apply suggestions from code review
    Co-authored-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- src/orchestrator.rs
+++ src/orchestrator.rs
@@ -177,7 +177,7 @@ impl ClassificationWithGenTask {
     }
 }
 
-/// Task for the /api/v2/text/task/detection/content endpoint
+/// Task for the /api/v2/text/detection/content endpoint
 #[derive(Debug)]
 pub struct TextContentDetectionTask {
     /// Request unique identifier
