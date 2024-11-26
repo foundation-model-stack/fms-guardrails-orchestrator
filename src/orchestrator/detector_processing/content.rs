4dd3d15f2a4e7d87464f77a0abe6a157ffd840b2
gkumbhat <kumbhat.gaurav@gmail.com>
2024-11-26 16:20:47 -0600

    :bug: Push detector processing module missed earlier
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

---
+++ src/orchestrator/detector_processing/content.rs
@@ -0,0 +1,52 @@
+/*
+ Copyright FMS Guardrails Orchestrator Authors
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+*/
+
+use crate::clients::detector::ContentAnalysisRequest;
+use crate::{
+    orchestrator::chat_completions_detection::ChatMessagesInternal,
+    models::ValidationError,
+    models::DetectorParams,
+    clients::openai::Content,
+};
+
+/// Function to get content analysis request from chat message by applying rules
+pub async fn request_from_chat_message(messages: ChatMessagesInternal, detector_params: DetectorParams) -> Result<ContentAnalysisRequest, ValidationError> {
+    // Implement content processing logic here
+    // Rules:
+    // Rule 1: Select last message from the list of messages
+    let message = messages.last().unwrap();
+
+    // Rule 2: Check if the message has content or not
+    if message.content.is_none() {
+        return Err(ValidationError::Invalid("Message at last index does not have content".into()));
+    }
+
+    // 3. Select if message is from role `user` or `assistant` otherwise return Err
+    match message.role.as_str() {
+       "user" | "assistant" => (),
+       _ => return Err(ValidationError::Invalid("Message at last index is not from user or assistant".into())),
+    }
+
+    let content = match message.content.clone().unwrap() {
+        Content::Text(text) => text,
+        _ => return Err(ValidationError::Invalid("Incorrect type requested".into()))
+    };
+    Ok(ContentAnalysisRequest::new(
+        vec![content],
+        detector_params,
+    ))
+}
