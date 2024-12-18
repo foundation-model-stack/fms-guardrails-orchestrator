bf1f4ec5b30dd8799e8e2785526498ad4bca0341
Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
2024-12-18 12:13:25 -0800

    :recycle: Use display
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>

--- src/clients.rs
+++ src/clients.rs
@@ -498,11 +498,11 @@ impl OnFailure<GrpcFailureClass> for ClientOnFailure {
 
         let (status_code, error) = match failure_classification {
             GrpcFailureClass::Code(code) => {
-                error!(?trace_id, code, latency_ms, "failure handling request",);
+                error!(%trace_id, code, latency_ms, "failure handling request",);
                 (Some(grpc_to_http_code(tonic::Code::from(code.get()))), None)
             }
             GrpcFailureClass::Error(error) => {
-                error!(?trace_id, latency_ms, "failure handling request: {}", error,);
+                error!(%trace_id, latency_ms, "failure handling request: {}", error,);
                 (None, Some(error))
             }
         };
