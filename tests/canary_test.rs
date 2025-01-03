9b03e5bd757b4c85713f6d488b68d122bd70e053
Mateus Devino <mdevino@ibm.com>
2025-01-03 11:27:34 -0300

    Refactor common integration test code
    Signed-off-by: Mateus Devino <mdevino@ibm.com>

--- tests/canary_test.rs
+++ tests/canary_test.rs
@@ -1,33 +1,12 @@
-use std::sync::Arc;
-
 use axum_test::TestServer;
-use fms_guardrails_orchestr8::{
-    config::OrchestratorConfig,
-    orchestrator::Orchestrator,
-    server::{get_health_app, ServerState},
-};
+use common::{ensure_global_rustls_state, shared_state, ONCE};
+use fms_guardrails_orchestr8::server::get_health_app;
 use hyper::StatusCode;
-use rustls::crypto::ring;
 use serde_json::Value;
-use tokio::sync::OnceCell;
 use tracing::debug;
 use tracing_test::traced_test;
 
-/// Async lazy initialization of shared state using tokio::sync::OnceCell
-static ONCE: OnceCell<Arc<ServerState>> = OnceCell::const_new();
-
-/// The actual async function that initializes the shared state if not already initialized
-async fn shared_state() -> Arc<ServerState> {
-    let config = OrchestratorConfig::load("tests/test.config.yaml")
-        .await
-        .unwrap();
-    let orchestrator = Orchestrator::new(config, false).await.unwrap();
-    Arc::new(ServerState::new(orchestrator))
-}
-
-fn ensure_global_rustls_state() {
-    let _ = ring::default_provider().install_default();
-}
+mod common;
 
 /// Checks if the health endpoint is working
 /// NOTE: We do not currently mock client services yet, so this test is
