9e3b67b04caff8924d4d18c063c9ec5e9c3fa3cc
Mateus Devino <mdevino@ibm.com>
2025-02-25 17:20:32 -0300

    test case: detection_on_generation::test_detector_returns_503()
    Signed-off-by: Mateus Devino <mdevino@ibm.com>

--- tests/detection_on_generation.rs
+++ tests/detection_on_generation.rs
@@ -20,6 +20,7 @@ use test_log::test;
 
 use common::{
     detectors::{ANSWER_RELEVANCE_DETECTOR, DETECTION_ON_GENERATION_DETECTOR_ENDPOINT},
+    errors::{DetectorError, OrchestratorError},
     orchestrator::{
         TestOrchestratorServer, ORCHESTRATOR_CONFIG_FILE_PATH,
         ORCHESTRATOR_DETECTION_ON_GENERATION_ENDPOINT,
@@ -162,6 +163,72 @@ async fn test_detection_above_default_threshold_is_returned() -> Result<(), anyh
             == DetectionOnGenerationResult {
                 detections: vec![detection.clone()]
             }
+    );
+
+    Ok(())
+}
+
+#[test(tokio::test)]
+async fn test_detector_returns_503() -> Result<(), anyhow::Error> {
+    let detector_name = ANSWER_RELEVANCE_DETECTOR;
+    let prompt = "In 2014, what was the average height of men who were born in 1996?";
+    let generated_text =
+        "The average height of men who were born in 1996 was 171cm (or 5'7.5'') in 2014.";
+    let detector_error = DetectorError {
+        code: 503,
+        message: "The detector is overloaded.".into(),
+    };
+
+    // Add detector mock
+    let mut mocks = MockSet::new();
+    mocks.insert(
+        MockPath::new(Method::POST, DETECTION_ON_GENERATION_DETECTOR_ENDPOINT),
+        Mock::new(
+            MockRequest::json(GenerationDetectionRequest {
+                prompt: prompt.into(),
+                generated_text: generated_text.into(),
+                detector_params: DetectorParams::new(),
+            }),
+            MockResponse::json(&detector_error).with_code(StatusCode::SERVICE_UNAVAILABLE),
+        ),
+    );
+
+    // Start orchestrator server and its dependencies
+    let mock_detector_server = HttpMockServer::new(detector_name, mocks)?;
+    let orchestrator_server = TestOrchestratorServer::run(
+        ORCHESTRATOR_CONFIG_FILE_PATH,
+        find_available_port().unwrap(),
+        find_available_port().unwrap(),
+        None,
+        None,
+        Some(vec![mock_detector_server]),
+        None,
+    )
+    .await?;
+
+    // Make orchestrator call
+    let response = orchestrator_server
+        .post(ORCHESTRATOR_DETECTION_ON_GENERATION_ENDPOINT)
+        .json(&DetectionOnGeneratedHttpRequest {
+            prompt: prompt.into(),
+            generated_text: generated_text.into(),
+            detectors: HashMap::from([(detector_name.into(), DetectorParams::new())]),
+        })
+        .send()
+        .await?;
+
+    debug!(?response);
+
+    // assertions
+    assert!(response.status() == StatusCode::SERVICE_UNAVAILABLE);
+    let response = response.json::<OrchestratorError>().await?;
+    assert!(response.code == detector_error.code);
+    assert!(
+        response.details
+            == format!(
+                "detector request failed for `{}`: {}",
+                detector_name, detector_error.message
+            )
     );
 
     Ok(())
