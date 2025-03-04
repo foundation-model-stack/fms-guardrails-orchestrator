76bac51c983b6b09841a76e8ff1c60ab63a24d38
declark1 <44146800+declark1@users.noreply.github.com>
2025-03-04 09:31:33 -0800

    Update MaxProcessedIndexBatcher doc string, add test_single_chunk_multiple_detectors
    Signed-off-by: declark1 <44146800+declark1@users.noreply.github.com>

--- src/orchestrator/types/detection_batcher/max_processed_index.rs
+++ src/orchestrator/types/detection_batcher/max_processed_index.rs
@@ -2,12 +2,21 @@ use std::collections::{btree_map, BTreeMap};
 
 use super::{Chunk, DetectionBatcher, Detections, DetectorId, InputId};
 
-/// A batcher based on the original "max processed index" aggregator.
+/// A batcher based on the original "max processed index"
+/// aggregator.
 ///
-/// Each chunk is a batch, returned in-order once detections
-/// from all detectors have been received for the chunk.
+/// A batch corresponds to a chunk. Batches are
+/// returned in-order as detections from all detectors
+/// are received for the chunk.
 ///
-/// Assumes all detectors are using the same chunker.
+/// For example, if we have n chunks with 3 detectors
+/// applied to each chunk, the first batch for chunk-1 is
+/// popped once detections from 3 detectors are received
+/// for chunk-1. The next batch for chunk-2 is popped once
+/// detections from 3 detectors are received for chunk-2,
+/// and so on.
+///
+/// This batcher requires that all detectors use the same chunker.
 pub struct MaxProcessedIndexBatcher {
     n: usize,
     state: BTreeMap<Chunk, Vec<Detections>>,
@@ -58,5 +67,85 @@ impl DetectionBatcher for MaxProcessedIndexBatcher {
             }
         }
         None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::orchestrator::types::Detection;
+
+    #[test]
+    fn test_single_chunk_multiple_detectors() {
+        let input_id = 0;
+        let chunk = Chunk {
+            input_start_index: 0,
+            input_end_index: 0,
+            start: 0,
+            end: 24,
+            text: "this is a dummy sentence".into(),
+        };
+
+        // Create a batcher that will process batches for 2 detectors
+        let n = 2;
+        let mut batcher = MaxProcessedIndexBatcher::new(n);
+
+        // Push detections for pii detector
+        batcher.push(
+            input_id,
+            "pii".into(),
+            chunk.clone(),
+            vec![Detection {
+                start: Some(5),
+                end: Some(10),
+                detector_id: Some("pii".into()),
+                detection_type: "pii".into(),
+                score: 0.4,
+                ..Default::default()
+            }]
+            .into(),
+        );
+
+        // We only have detections for 1 detector
+        // pop_batch() should return None
+        assert!(batcher.pop_batch().is_none());
+
+        // Push detections for hap detector
+        batcher.push(
+            input_id,
+            "hap".into(),
+            chunk.clone(),
+            vec![
+                Detection {
+                    start: Some(5),
+                    end: Some(10),
+                    detector_id: Some("hap".into()),
+                    detection_type: "hap".into(),
+                    score: 0.8,
+                    ..Default::default()
+                },
+                Detection {
+                    start: Some(15),
+                    end: Some(20),
+                    detector_id: Some("hap".into()),
+                    detection_type: "hap".into(),
+                    score: 0.8,
+                    ..Default::default()
+                },
+            ]
+            .into(),
+        );
+
+        // We have detections for 2 detectors
+        // pop_batch() should return a batch containing 3 detections for the chunk
+        let batch = batcher.pop_batch();
+        assert!(
+            batch.is_some_and(|(chunk, detections)| { chunk == chunk && detections.len() == 3 })
+        );
+    }
+
+    #[test]
+    fn test_out_of_order_chunks() {
+        todo!()
     }
 }
