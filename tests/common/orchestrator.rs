adceea535242f1ad48f04969fd208d3f73e687eb
Paulo Marques Caldeira Junior <7291154+pmcjr@users.noreply.github.com>
2025-03-14 17:48:58 -0300

    Integration tests for `/api/v1/task/classification-with-text-generation` (#340)

--- tests/common/orchestrator.rs
+++ tests/common/orchestrator.rs
@@ -40,6 +40,7 @@ pub const ORCHESTRATOR_CONFIG_FILE_PATH: &str = "tests/test_config.yaml";
 // Endpoints
 pub const ORCHESTRATOR_STREAMING_ENDPOINT: &str =
     "/api/v1/task/server-streaming-classification-with-text-generation";
+pub const ORCHESTRATOR_UNARY_ENDPOINT: &str = "/api/v1/task/classification-with-text-generation";
 pub const ORCHESTRATOR_CONTENT_DETECTION_ENDPOINT: &str = "/api/v2/text/detection/content";
 pub const ORCHESTRATOR_DETECTION_ON_GENERATION_ENDPOINT: &str = "/api/v2/text/detection/generated";
 pub const ORCHESTRATOR_CONTEXT_DOCS_DETECTION_ENDPOINT: &str = "/api/v2/text/detection/context";
