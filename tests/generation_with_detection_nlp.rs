6960a73fa1167cfb413984afc3b0c7812533034b
Mateus Devino <mdevino@ibm.com>
2025-03-18 14:48:53 -0300

    test case: generation_with_detection_nlp::detections()
    Signed-off-by: Mateus Devino <mdevino@ibm.com>

--- tests/generation_with_detection_nlp.rs
+++ tests/generation_with_detection_nlp.rs
@@ -121,3 +121,85 @@ async fn no_detections() -> Result<(), anyhow::Error> {
 
     Ok(())
 }
+
+/// Asserts detections above default threshold are returned.
+#[test(tokio::test)]
+async fn detections() -> Result<(), anyhow::Error> {
+    let detector_name = ANSWER_RELEVANCE_DETECTOR;
+    let prompt = "In 2014, what was the average height of men who were born in 1996?";
+    let generated_text =
+        "The average height of men who were born in 1996 was 171cm (or 5'7.5'') in 2014.";
+    let detection = DetectionResult {
+        detection_type: "relevance".into(),
+        detection: "is_relevant".into(),
+        detector_id: Some(detector_name.into()),
+        score: 0.89,
+        evidence: None,
+    };
+
+    // Add generation mock
+    let model_id = "my-super-model-8B";
+
+    let mut generation_mocks = MockSet::new();
+    generation_mocks.mock(|when, then| {
+        when.path(GENERATION_NLP_UNARY_ENDPOINT)
+            .header(GENERATION_NLP_MODEL_ID_HEADER_NAME, model_id)
+            .pb(TextGenerationTaskRequest {
+                text: prompt.into(),
+                ..Default::default()
+            });
+        then.pb(GeneratedTextResult {
+            generated_text: generated_text.into(),
+            ..Default::default()
+        });
+    });
+
+    // Add input detection mock
+    let mut detection_mocks = MockSet::new();
+    detection_mocks.mock(|when, then| {
+        when.post()
+            .path(DETECTION_ON_GENERATION_DETECTOR_ENDPOINT)
+            .json(GenerationDetectionRequest {
+                prompt: prompt.into(),
+                generated_text: generated_text.into(),
+                detector_params: DetectorParams::new(),
+            });
+        then.json(vec![detection.clone()]);
+    });
+
+    // Start orchestrator server and its dependencies
+    let generation_server = MockServer::new("nlp").grpc().with_mocks(generation_mocks);
+    let mock_detector_server = MockServer::new(detector_name).with_mocks(detection_mocks);
+    let orchestrator_server = TestOrchestratorServer::builder()
+        .config_path(ORCHESTRATOR_CONFIG_FILE_PATH)
+        .generation_server(&generation_server)
+        .detector_servers([&mock_detector_server])
+        .build()
+        .await?;
+
+    // Make orchestrator call
+    let response = orchestrator_server
+        .post(ORCHESTRATOR_GENERATION_WITH_DETECTION_ENDPOINT)
+        .json(&GenerationWithDetectionHttpRequest {
+            model_id: model_id.into(),
+            prompt: prompt.into(),
+            detectors: HashMap::from([(detector_name.into(), DetectorParams::new())]),
+            text_gen_parameters: None,
+        })
+        .send()
+        .await?;
+    debug!("{response:#?}");
+
+    // assertions
+    assert_eq!(response.status(), StatusCode::OK);
+    assert_eq!(
+        response.json::<GenerationWithDetectionResult>().await?,
+        GenerationWithDetectionResult {
+            generated_text: generated_text.into(),
+            detections: vec![detection.clone()],
+            input_token_count: 0
+        }
+    );
+
+    Ok(())
+}
